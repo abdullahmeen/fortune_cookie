5b12869de10ff67d8b1697ee4ae878177dbc8124
abdullahmeen <abdullahamen321@gmail.com>
2025-07-16 22:18:02 +0500

    fortune_cookie

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+  String _currentFortune = "";
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _currentFortune = '';
+  final _fortuneList = [
+    "you will meet a new friend",
+    "you will go to kumrat valley",
+    "this is meant for you",
+    "no one is good unless you help them",
+    "grow up and be kind",
+  ];
+
+  void _randomFortune() {
+    var random = Random();
+    int fortune = random.nextInt(_fortuneList.length);
+    setState(() {
+    _currentFortune = _fortuneList[fortune];
+      
+    });
+    // print(fortune);
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text("Flutter Demo Home Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Your Fortune is:"),
+            Text(
+              '${_currentFortune}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _randomFortune,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
